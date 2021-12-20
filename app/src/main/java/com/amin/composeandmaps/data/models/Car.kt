4bbf3972b26cb8f265604b11488c0fa51bab4948
Amin Keshavarzian <ak@next-munich.com>
2021-12-20 21:53:28 +0330

    added car network model

--- app/src/main/java/com/amin/composeandmaps/data/models/Car.kt
+++ app/src/main/java/com/amin/composeandmaps/data/models/Car.kt
@@ -16,11 +16,26 @@ data class Car(
     val modelName: String,
     val name: String,
     val series: String,
-    val transmission: String) {
-
+    val transmission: String
+) {
     companion object {
         fun mock() = Car(
-            
+            id = "WMWSW31030T222518",
+            modelIdentifier = "mini",
+            modelName = "MINI",
+            name = "Vanessa",
+            make = "BMW",
+            group = "MINI",
+            color = "midnight_black",
+            series = "MINI",
+            fuelType = "D",
+            fuelLevel = 0.7,
+            transmission = "M",
+            licensePlate = "M-VO0259",
+            latitude = 48.134557,
+            longitude = 11.576921,
+            innerCleanliness = "REGULAR",
+            carImageUrl = "https://cdn.sixt.io/codingtask/images/mini.png"
         )
     }
 }
